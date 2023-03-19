f180173d0e6621a0fe9ca7cdbe2801035e65a43a
Adonis Najimi <adopi.naj@gmail.com>
2023-03-19 22:35:56 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 curl -X POST -d '{"ipv4": "172.0.1.3", "desc": "local1"}' -H "Content-Type:application/json"  http://localhost:8000/api/ips
-curl -X POST -d '{"ipv4": "172.0.1.2", "desc": "local2"}' -H "Content-Type:application/json"  http://localhost:8000/api/ips
+curl -X POST -d '{"ipv4": "222.0.1.2", "desc": "local2"}' -H "Content-Type:application/json"  http://localhost:8000/api/ips
+curl -X POST -d '{"ipv4": "333.0.1.2", "desc": "local2"}' -H "Content-Type:application/json"  http://localhost:8000/api/ips
+curl -X POST -d '{"ipv4": "444.0.1.2", "desc": "local2"}' -H "Content-Type:application/json"  http://localhost:8000/api/ips
+curl -X POST -d '{"ipv4": "555.0.1.2", "desc": "local2"}' -H "Content-Type:application/json"  http://localhost:8000/api/ips
 # uniqueness
 curl -X POST -d '{"ipv4": "172.0.1.2", "desc": "local2"}' -H "Content-Type:application/json"  http://localhost:8000/api/ips
 
@@ -7,4 +10,6 @@ echo ""
 curl -X GET http://localhost:8000/api/ips
 echo "TXT"
 curl -X GET http://localhost:8000/api/ips.txt
+curl -X GET http://localhost:8000/api/ips.txt -o ip.txt
 echo ""
+cat ip.txt
